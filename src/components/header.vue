9e4fb8f5112556b891050f8e349323ed8c953671
F0eiVer <arefev030303@mail.ru>
2023-02-04 09:32:13 +1000

    foo

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="header">
     <div class="header__img__section">
-      <RouterLink to="/"><img id="img" class="header__img" src="../images/DS2017_TP09_black.png" /></RouterLink>
-    </div>
-    <div class="header__margin_right">
-      <div class="header__section">
-        <button class="header__button">О компании</button>
-      </div>
+      <RouterLink to="/"
+        ><img
+          id="img"
+          class="header__img"
+          src="../images/DS2017_TP09_black.png"
+      /></RouterLink>
     </div>
   </div>
 </template>
